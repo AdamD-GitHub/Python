a438acb8381b616897947dd900c1a70f36009292
AdamD-GitHub <49349488+AdamD-GitHub@users.noreply.github.com>
2020-10-05 14:07:13 -0500

    Update PythonNotes_09_MyProgram.py

--- PythonNotes_09_MyProgram.py
+++ PythonNotes_09_MyProgram.py
@@ -14,7 +14,7 @@ print("\n-------\n")
 #---------------------------------------------
 #IMPORT FROM A MODULE
 
-from PythonNotes_09_MyModule import my_func
+from PythonNotes_09_MyModule import my_func		#in same directory as main (main)
 
 my_func()
 
@@ -23,8 +23,8 @@ print("\n-------\n")
 #---------------------------------------------
 #IMPORT FROM A PACKAGE AND SUBPACKAGE
 
-from MyMainPackage import some_main_script
-from MyMainPackage.SubPackage import mysubscript
+from MyMainPackage import some_main_script		#one directory down from main (main..sub)
+from MyMainPackage.SubPackage import mysubscript	#two directory down from main (main..sub..subsub)
 
 some_main_script.report_main()
 mysubscript.report_sub()
@@ -32,6 +32,7 @@ mysubscript.report_sub()
 #---------------------------------------------
 print("\n-------\n")
 #---------------------------------------------
+#__name__ == 'main": since Python doesn't have a main, this can be used to determine the main code.  
 
 if __name__ == '__main__':
 	print("PythonNotes_09_MyProgram.py is being run directly")
